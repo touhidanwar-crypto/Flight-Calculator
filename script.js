0a45f2a5e6f44c3f46ff8ae633e1490bc2b4fa77
touhidanwar-crypto <touhid.anwar@gozayaan.com>
2026-02-04 23:14:43 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+const display = document.getElementById("display");
+
+function append(value) {
+  display.value += value;
+}
+
+function clearDisplay() {
+  display.value = "";
+}
+
+function calculate() {
+  display.value = eval(display.value);
+}
